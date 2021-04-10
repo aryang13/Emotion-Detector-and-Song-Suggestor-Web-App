97fa2c3367dc9e69d16cf3b548a51c331f49ba8c
aryaphan <aryaphan31@gmail.com>
2021-04-09 19:52:04 -0700

    testing homepage

--- final_project/webpage/urls.py
+++ final_project/webpage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='webpage-home'),
+   # path('', views.home, name='webpage-home'),
+    path('', views.homepage, name='homepage'),
     path('about/', views.about, name='webpage-about'),
 ]
