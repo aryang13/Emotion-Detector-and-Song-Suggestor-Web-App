8f6812defd38c33314dfd11da13fa153898904de
Steve He <2659871636@qq.com>
2021-05-09 01:55:23 +0800

    crop working

--- final_project/webpage/views.py
+++ final_project/webpage/views.py
@@ -11,8 +11,64 @@ from torchvision import datasets, models, transforms
 import tensorflow as tf
 import os
 from PIL import Image
-
+import mtcnn
 # Create your views here.
+
+#The MTCNN takes as a input a single image and outputs the bounding box coordinates for that image where the face is.
+#The code below takes in an input directory and runs the MTCNN on all the images in that directory and outputs in
+#output directory only the bounded images (only faces).
+#link to code below: https://stackoverflow.com/questions/65105644/how-to-face-extraction-from-images-in-a-folder-with-mtcnn-in-python
+from mtcnn import MTCNN
+import cv2
+import os
+def crop_image(source_dir, dest_dir, mode):
+    if os.path.isdir(dest_dir)==False:
+        os.mkdir(dest_dir)
+    detector = MTCNN()
+    source_list=os.listdir(source_dir)
+    uncropped_file_list=[]
+    for f in source_list:
+        f_path=os.path.join(source_dir, f)
+        dest_path=os.path.join(dest_dir,f)
+        img=cv2.imread(f_path)
+        try:
+            data=detector.detect_faces(img)
+        except:
+            print(f_path)
+            data==[]
+            pass
+        if data ==[]:
+            uncropped_file_list.append(f_path)
+        else:
+            if mode==1:  #detect the box with the largest area
+                for i, faces in enumerate(data): # iterate through all the faces found
+                    box=faces['box']  # get the box for each face
+                    biggest=0
+                    area = box[3]  * box[2]
+                    if area>biggest:
+                        biggest=area
+                        bbox=box
+                bbox[0]= 0 if bbox[0]<0 else bbox[0]
+                bbox[1]= 0 if bbox[1]<0 else bbox[1]
+                img=img[bbox[1]: bbox[1]+bbox[3],bbox[0]: bbox[0]+ bbox[2]]
+                cv2.imwrite(dest_path, img)
+            else:
+                for i, faces in enumerate(data): # iterate through all the faces found
+                    box=faces['box']
+                    if box !=[]:
+                        # return all faces found in the image
+                        box[0]= 0 if box[0]<0 else box[0]
+                        box[1]= 0 if box[1]<0 else box[1]
+                        cropped_img=img[box[1]: box[1]+box[3],box[0]: box[0]+ box[2]]
+                        fname=os.path.splitext(f)[0]
+                        fext=os.path.splitext(f)[1]
+                        fname=fname + str(i) + fext
+                        save_path=os.path.join(dest_dir,fname )
+                        cv2.imwrite(save_path, cropped_img)
+
+    return uncropped_file_list
+
+
 
 def homepage(request):
 
@@ -30,13 +86,16 @@ def homepage(request):
 
 def write_image(image):
     cv2.imwrite("webpage\\images\\image.png", image)
+    source_dir = 'webpage/images'
+    dest_dir = 'webpage/faceonly'
+    uncropped_files_list=crop_image(source_dir, dest_dir,1)
 
 def music(request, img):
     write_image(img)
     model=torch.load('projectmod',map_location=torch.device('cpu'))
     model.eval()
     xform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
-    img=Image.open('webpage\\images\\image.png')
+    img=Image.open('webpage\\faceonly\\image.png')
     data = xform(img)
     #data = np.moveaxis(img, -1, 0)
     #data=torch.from_numpy(data).float()
