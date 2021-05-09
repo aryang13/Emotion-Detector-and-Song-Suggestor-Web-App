e331f4e59429d06ebb791ff5233ca94daedee157
aryaphan <aryaphan31@gmail.com>
2021-05-09 14:18:56 -0700

    designed about page. put dummy text for testing

--- final_project/webpage/urls.py
+++ final_project/webpage/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('music/', views.music, name='music'),
-    path('about/', views.about, name='webpage-about'),
+    path('about/', views.about, name='about'),
    # path('', views.home, name='webpage-home'),
     # path('', views.my_image, name='webpage-home'),
     # path('my_image/', views.my_image, name='main'),
