b726d9219b7b4e1c7094194da015275e29eef698
aryaphan <aryaphan31@gmail.com>
2021-05-15 19:38:13 -0700

    cleaned up

--- final_project/webpage/urls.py
+++ final_project/webpage/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('music/', views.music, name='music'),
     path('about/', views.about, name='about'),
-   # path('', views.home, name='webpage-home'),
-    # path('', views.my_image, name='webpage-home'),
-    # path('my_image/', views.my_image, name='main'),
 ]
